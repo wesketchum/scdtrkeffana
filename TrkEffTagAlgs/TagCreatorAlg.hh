0b459cae360c9a1f56ccc16d7e811aae076a0bc4
Wesley Ketchum <wketchum@fnal.gov>
2016-01-14 18:15:57 +0100

    starting to fill out functions in TagCreatorAlg

--- TrkEffTagAlgs/TagCreatorAlg.hh
+++ TrkEffTagAlgs/TagCreatorAlg.hh
@@ -2,8 +2,13 @@
 #define TRKEFF_TAGCREATORALG_H
 
 #include <string>
+#include <vector>
+
+#include "fhiclcpp/ParameterSet.h"
 
 #include "TTree.h"
+
+#include "RecoBase/Hit.h"
 
 namespace trkeff{
   class TagCreatorAlg;
@@ -16,6 +21,10 @@ public:
   /// Default constructor
   TagCreatorAlg();
 
+  void Configure( fhicl::ParameterSet const& );
+
+  void CreateTags( std::vector<recob::Hit> const&);
+  
   /// Default destructor
   virtual ~TagCreatorAlg(){};
 
@@ -23,6 +32,17 @@ public:
   
  private:
 
+  std::vector< std::vector<float> > fSearchRegions; //vector of [y1, z1, y2, z2]
+  std::vector< int >                fTagWiresPerPlane; //search size, in wires, for each plane
+  float                             fLinearity; //min linearity for tag
+  std::vector< std::string >        fPlaneCombinations; //allowed plane combinations
+  double                            fTimeMatch; //time matching condition for hits across planes
+
+  std::vector< std::vector< std::vector<size_t> > > fSortedHitsIndex;
+  void SortHitsBySearchRegion(std::vector<recob::Hit> const&);
+
+  std::vector<size_t> ClusterHits( std::vector<size_t> const& );
+  
   TTree*      fTree;
   
 };
