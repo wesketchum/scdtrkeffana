9618d9283984f9242e4b01dcd0ede21542d85b6e
Wesley Ketchum <wketchum@fnal.gov>
2016-01-15 10:42:56 +0100

    start filling out tag creator configuration

--- TrkEffTagAlgs/TagCreatorAlg.cxx
+++ TrkEffTagAlgs/TagCreatorAlg.cxx
@@ -1,8 +1,15 @@
 #ifndef TRKEFF_TAGCREATORALG_CXX
 #define TRKEFF_TAGCREATORALG_CXX
 
-#include "TagCreatorAlg.hh"
 #include <iostream>
+
+#include "cetlib/exception.h"
+
+#include "RecoBase/Hit.h"
+#include "SimpleTypesAndConstants/geo_types.h"
+#include "Geometry/GeometryCore.h"
+
+#include "TagCreatorAlg.hh"
 
 trkeff::TagCreatorAlg::TagCreatorAlg()
 {}
@@ -11,5 +18,67 @@ void trkeff::TagCreatorAlg::SetupOutputTree(TTree* tfs_tree){
   fTree = tfs_tree;
   fTree->SetObject(fTree->GetName(),"TagCreatorAlg Tree");
 }
+
+void trkeff::TagCreatorAlg::FillConfigParameters(fhicl::ParameterSet const& p){
+  fSearchRegions = p.get< std::vector< std::vector<float> > >("SearchRegions");
+  fTagWiresPerPlane = p.get< std::vector<unsigned int> >("TagWiresPerPlane");
+  fLineMaxChiSquare = p.get< double >("LineMaxChiSquare");
+  fPlaneCombinations = p.get< std::vector<std::string> >("PlaneCombinations");
+  fTimeMatch = p.get< double >("TimeMatch");
+  fMinHitAmplitudes = p.get< std::vector<double> >("MinHitAmplitudes");
+  fMaxHitAmplitudes = p.get< std::vector<double> >("MaxHitAmplitudes");
+}
+
+void trkeff::TagCreatorAlg::ProcessConfigParameters(geo::GeometryCore const& geo){
+
+  if(fLineMaxChiSquare<=0)
+    throw cet::exception("trkeff::TagCreatorAlg::Configure")
+      << "LineMaxChiSquare must be greater than zero\n";
+  if(fTimeMatch<=0)
+    throw cet::exception("trkeff::TagCreatorAlg::Configure")
+      << "TimeMatch must be greater than zero\n";
+
+  if(fTagWiresPerPlane.size()==1)
+    fTagWiresPerPlane = std::vector<unsigned int>(geo.Nplanes(),fTagWiresPerPlane[0]);
+  if(fTagWiresPerPlane.size()!=geo.Nplanes())
+    throw cet::exception("trkeff::TagCreatorAlg::Configure")
+      << "TagWiresPerPlane must have size Nplanes=" << geo.Nplanes() << ".\n";
+
+  if(fMinHitAmplitudes.size()==1)
+    fMinHitAmplitudes = std::vector<double>(geo.Nplanes(),fMinHitAmplitudes[0]);
+  if(fMinHitAmplitudes.size()!=geo.Nplanes())
+    throw cet::exception("trkeff::TagCreatorAlg::Configure")
+      << "fMinHitAmplitudes must have size Nplanes=" << geo.Nplanes() << ".\n";
+
+  if(fMaxHitAmplitudes.size()==1)
+    fMaxHitAmplitudes = std::vector<double>(geo.Nplanes(),fMaxHitAmplitudes[0]);
+  if(fMaxHitAmplitudes.size()!=geo.Nplanes())
+    throw cet::exception("trkeff::TagCreatorAlg::Configure")
+      << "fMinHitAmplitudes must have size Nplanes=" << geo.Nplanes() << ".\n";
+
+  for(size_t i_p=0; i_p<geo.Nplanes(); ++i_p){
+    if(fMaxHitAmplitudes[i_p]<fMinHitAmplitudes[i_p])
+      throw cet::exception("trkeff::TagCreatorAlg::Configure")
+	<< "MaxHitAmplitude must be greater than MinHitAmplitude\n";
+    if(fMinHitAmplitudes[i_p]<0)
+      throw cet::exception("trkeff::TagCreatorAlg::Configure")
+	<< "MinHitAmplitude must be greater than zero\n";
+  }
+  
+  TranslateSearchRegions(geo);
+  //ValidatePlaneCombinations();
+  
+    
+  
+}
+
+void trkeff::TagCreatorAlg::TranslateSearchRegions(geo::GeometryCore const&){
+}
+
+void trkeff::TagCreatorAlg::Configure( fhicl::ParameterSet const& p, geo::GeometryCore const& geo){
+  FillConfigParameters(p);
+  ProcessConfigParameters(p);
+}
+
 
 #endif
--- TrkEffTagAlgs/TagCreatorAlg.hh
+++ TrkEffTagAlgs/TagCreatorAlg.hh
@@ -8,7 +8,11 @@
 
 #include "TTree.h"
 
-#include "RecoBase/Hit.h"
+//#include "RecoBase/Hit.h"
+//#include "SimpleTypesAndConstants/geo_types.h"
+
+namespace recob{ class Hit; }
+namespace geo{ struct WireID; class GeometryCore; }
 
 namespace trkeff{
   class TagCreatorAlg;
@@ -21,7 +25,7 @@ public:
   /// Default constructor
   TagCreatorAlg();
 
-  void Configure( fhicl::ParameterSet const& );
+  void Configure( fhicl::ParameterSet const&, geo::GeometryCore const& );
 
   void CreateTags( std::vector<recob::Hit> const&);
   
@@ -32,15 +36,24 @@ public:
   
  private:
 
-  std::vector< std::vector<float> > fSearchRegions; //vector of [y1, z1, y2, z2]
-  std::vector< int >                fTagWiresPerPlane; //search size, in wires, for each plane
-  float                             fLinearity; //min linearity for tag
-  std::vector< std::string >        fPlaneCombinations; //allowed plane combinations
-  double                            fTimeMatch; //time matching condition for hits across planes
+  //configs from fhicl file
+  std::vector< std::vector<float> > fSearchRegions;     // vector of [y1, z1, y2, z2]
+  std::vector< unsigned int >       fTagWiresPerPlane;  // search size, in wires, for each plane
+  double                            fLineMaxChiSquare;  // max chi squared allowed, per dof, for tag hits being in a line
+  std::vector< std::string >        fPlaneCombinations; // allowed plane combinations
+  double                            fTimeMatch;         // time matching condition for hits across planes
+  std::vector< double >             fMinHitAmplitudes;  // min hit amplitudes per plane
+  std::vector< double >             fMaxHitAmplitudes;  // max hit ampltidues per plane
 
+  //internal data  
+  std::vector< std::vector<geo::WireID> > fSearchRegionsWires;
   std::vector< std::vector< std::vector<size_t> > > fSortedHitsIndex;
-  void SortHitsBySearchRegion(std::vector<recob::Hit> const&);
 
+  //internal functions
+  void FillConfigParameters(fhicl::ParameterSet const&);
+  void ProcessConfigParameters(geo::GeometryCore const&);
+  void TranslateSearchRegions(geo::GeometryCore const&);
+  void SortHitsBySearchRegion(std::vector<recob::Hit> const&);
   std::vector<size_t> ClusterHits( std::vector<size_t> const& );
   
   TTree*      fTree;
