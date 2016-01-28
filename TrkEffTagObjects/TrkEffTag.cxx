c1915d7bc9880d5a5db3e42b0207a5a15be49e44
Wesley Ketchum <wketchum@fnal.gov>
2016-01-28 07:44:42 +0100

    add in thetaxz and thetayz, and hope I am calculating angles correctly

--- TrkEffTagObjects/TrkEffTag.cxx
+++ TrkEffTagObjects/TrkEffTag.cxx
@@ -1,6 +1,37 @@
 #include "TrkEffTagObjects/TrkEffTag.h"
 
+#include <math.h>
+
 void trkeff::TrkEffTag::CalculateAngles(){
 
-  
+  if(fStartPoint[0]==fEndPoint[0] &&
+     fStartPoint[1]==fEndPoint[1])
+    fPhi=0;				      
+  else
+    fPhi = std::atan2(fEndPoint[1]-fStartPoint[1],
+		      fEndPoint[0]-fStartPoint[0]);
+
+  if(fStartPoint[0]==fEndPoint[0] &&
+     fStartPoint[1]==fEndPoint[1] &&
+     fStartPoint[2]==fEndPoint[2])
+    fTheta=0;
+  else
+    fTheta = std::atan2(std::sqrt((fEndPoint[1]-fStartPoint[1])*(fEndPoint[1]-fStartPoint[1]) +
+				  (fEndPoint[0]-fStartPoint[0])*(fEndPoint[0]-fStartPoint[0])),
+			fEndPoint[2]-fStartPoint[2]);
+
+  if(fStartPoint[1]==fEndPoint[1] &&
+     fStartPoint[2]==fEndPoint[2])
+    fThetaYZ=0;				      
+  else
+    fThetaYZ = std::atan2(fEndPoint[1]-fStartPoint[1],
+			  fEndPoint[2]-fStartPoint[2]);
+
+  if(fStartPoint[0]==fEndPoint[0] &&
+     fStartPoint[2]==fEndPoint[2])
+    fThetaXZ=0;				      
+  else
+    fThetaXZ = std::atan2(fEndPoint[2]-fStartPoint[2],
+			  fEndPoint[0]-fStartPoint[0]);
+
 }
